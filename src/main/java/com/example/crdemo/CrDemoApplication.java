e155f29c74e95ec3468a36dbde8537ee2c63c51e
Harish K <hckukatla@gmail.com>
2022-09-21 15:28:27 -0400

    Initial Commit

---
+++ src/main/java/com/example/crdemo/CrDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crdemo/CrDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
